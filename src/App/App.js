19a7d0591681174191b34461fab3a0db8504edd7
Jakeeeeeeeeeeeee <yangjinchen1995@gmail.com>
2021-06-08 03:24:50 +1000

    feat(HomePage): introducing the Home Page

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
+import HomePage from './Pages/HomePage';
 
 function App() {
   return (
-    <div>
-      Hello
-    </div>
+    <>
+      <HomePage />
+    </>
   );
 }
 
