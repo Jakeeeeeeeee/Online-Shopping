f174a038548a89198c14b96cf4b33fa8b53edced
Jakeeeeeeeeeeeee <yangjinchen1995@gmail.com>
2021-06-09 03:41:30 +1000

    feat(Router): introducing the React Router

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
 import HomePage from './Pages/HomePage';
+import { Route } from 'react-router-dom';
+import HatsPage from './Pages/HatsPage';
 
 function App() {
   return (
     <>
-      <HomePage />
+      <Route exact="true" path="/" component={HomePage} />
+      <Route path='/hats' component={HatsPage} />
     </>
   );
 }
