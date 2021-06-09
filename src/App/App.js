3e9aa0fc4c48c403d0ca0e9a249fdcc186b1bce8
Jakeeeeeeeeeeeee <yangjinchen1995@gmail.com>
2021-06-10 00:44:31 +1000

    feat(Header): introducing the Header component

--- src/App/App.js
+++ src/App/App.js
@@ -4,10 +4,12 @@ import HomePage from './Pages/HomePage';
 import { Route } from 'react-router-dom';
 import HatsPage from './Pages/HatsPage';
 import ShopPage from './Pages/ShopPage';
+import Header from './components/Header';
 
 function App() {
   return (
     <>
+      <Header />
       <Route exact="true" path="/" component={HomePage} />
       <Route path="/hats" component={HatsPage} />
       <Route path="/shop" component={ShopPage} />
