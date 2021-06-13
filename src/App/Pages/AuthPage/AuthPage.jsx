e25d1af36797d4897065a931dd8fd88204d73a62
Jakeeeeeeeeeeeee <yangjinchen1995@gmail.com>
2021-06-14 03:55:09 +1000

    feat(AuthPage): introducing the styles for the Auth Page

--- src/App/Pages/AuthPage/AuthPage.jsx
+++ src/App/Pages/AuthPage/AuthPage.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
-import SignIn from '../../components/SignIn';
+import SignIn from './components/SignIn';
+import SignUp from './components/SignUp';
+import './authPage.styles.scss';
 
 const AuthPage = () => (
   <div className="sign-in-and-sign-up">
     <SignIn />
+    <SignUp />
   </div>
 );
 
