39c5419bbeece7f499d1f18d35a4a603b67cac66
Jakeeeeeeeeeeeee <yangjinchen1995@gmail.com>
2021-06-22 01:16:38 +1000

    feat(CheckoutPage): introducing the CheckoutPage and onclick funtion in CartDropdown

--- src/App/components/Header/components/CartDropdown/CartDropdown.jsx
+++ src/App/components/Header/components/CartDropdown/CartDropdown.jsx
@@ -4,22 +4,34 @@ import './cartDropdown.styles.scss';
 import CartItem from './components/CartItem';
 import { connect } from 'react-redux';
 import { selectCartItems } from '../../../../../redux/cart/cartSelectors';
+import { createStructuredSelector } from 'reselect';
+import { withRouter } from 'react-router-dom';
+import { toggleCartHidden } from '../../../../../redux/cart/cartActions/cartActions';
 
-const CartDropDown = ({ cartItems }) => (
+
+const CartDropDown = ({ cartItems, history, dispatch }) => (
   <div className="cart-dropdown">
     <div className="cart-items">
-      {
+      {cartItems.length ? (
         cartItems.map(cartItem => (
           <CartItem key={CartItem.id} item={cartItem} />
         ))
+      ) : (
+        <span className="empty-message">Your cart is empty</span>
+      )
       }
     </div>
-    <Button>GO TO CHECKOUT</Button>
+    <Button onClick={() => {
+      history.push('/checkout');
+      dispatch(toggleCartHidden())
+    }}>
+      GO TO CHECKOUT
+    </Button>
   </div>
 )
 
-const mapStateToProps = (state) => ({
-  cartItems: selectCartItems(state)
+const mapStateToProps = createStructuredSelector({
+  cartItems: selectCartItems
 });
 
-export default connect(mapStateToProps)(CartDropDown);
+export default withRouter(connect(mapStateToProps)(CartDropDown));
