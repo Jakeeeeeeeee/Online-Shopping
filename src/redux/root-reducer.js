7bc32847f70cc11702368ff031b895e1c9c698d2
Jakeeeeeeeeeeeee <yangjinchen1995@gmail.com>
2021-06-22 02:14:00 +1000

    feat(CheckoutItem): introducing the functionality of increasing and decreasing the item quantities

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,6 +1,6 @@
 import { combineReducers } from "redux";
 import userReducer from "./user/userReducer/userReducer";
-import cartReducer from "./cart/cartReducer/carReducer";
+import cartReducer from "./cart/cartReducer/cartReducer";
 
 export default combineReducers({
   user: userReducer,
