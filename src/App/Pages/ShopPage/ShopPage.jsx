3a1871c642ca504d5d20118df9a5b38d4a46dd7a
Jakeeeeeeeeeeeee <yangjinchen1995@gmail.com>
2021-06-27 02:01:33 +1000

    feat(CollectionPage): introducing the CollectionPage and clicking the relevant url to the Page

--- src/App/Pages/ShopPage/ShopPage.jsx
+++ src/App/Pages/ShopPage/ShopPage.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import CollectionOverview from './components/CollectionOverview';
+import { Route } from 'react-router-dom';
+import CollectionPage from './components/CollectionPage/CollectionPage';
 
-const ShopPage = ({collections}) => {
+const ShopPage = ({ match }) => {
   return (
     <div className="shop-page">
-      <CollectionOverview />
+      <Route exact path={`${match.path}`} component={CollectionOverview} />
+      <Route exact path={`${match.path}/:collectionId`} component={CollectionPage} />
     </div>
   )
 };
--- src/redux/directoryReducer/directoryReducer.js
+++ src/redux/directoryReducer/directoryReducer.js
@@ -4,19 +4,19 @@ const INITIAL_STATE = {
       title: 'hats',
       imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
       id: 1,
-      linkUrl: 'hats'
+      linkUrl: 'shop/hats'
     },
     {
       title: 'jackets',
       imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
       id: 2,
-      linkUrl: '/jackets'
+      linkUrl: 'shop/jackets'
     },
     {
       title: 'sneakers',
       imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
       id: 3,
-      linkUrl: '/sneakers'
+      linkUrl: 'shop/sneakers'
     },
     {
       title: 'womens',
@@ -24,7 +24,7 @@ const INITIAL_STATE = {
       size: 'large',
       id: 4,
       size: 'large',
-      linkUrl: '/womens'
+      linkUrl: 'shop/womens'
     },
     {
       title: 'mens',
@@ -32,7 +32,7 @@ const INITIAL_STATE = {
       size: 'large',
       id: 5,
       size: 'large',
-      linkUrl: '/mens'
+      linkUrl: 'shop/mens'
     }
   ]
 };
