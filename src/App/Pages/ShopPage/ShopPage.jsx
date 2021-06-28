32e4ab14ab1e173203d0a9d64be382effe4b4372
Jakeeeeeeeeeeeee <yangjinchen1995@gmail.com>
2021-06-29 05:00:16 +1000

    feat(withSpinner): deleting the initial shop value from state and introducing the withSpinner for the loading state when feching data from backend

--- src/App/Pages/ShopPage/ShopPage.jsx
+++ src/App/Pages/ShopPage/ShopPage.jsx
@@ -5,26 +5,45 @@ import CollectionPage from './components/CollectionPage/CollectionPage';
 import { firestore, convertCollectionsSnapshotToMap } from '../../../firebase/firebase.utils';
 import { updateCollections } from '../../../redux/shop/shopActions';
 import { connect } from 'react-redux';
+import WithSpinner from '../../components/withSpinner/withSpinner';
+
+const CollectionsOverviewWithSpinner = WithSpinner(CollectionOverview);
+const CollectionPageWithSpinner = WithSpinner(CollectionPage);
 
 class ShopPage extends React.Component {
+  state = {
+    loading: true
+  };
+
   unsubscribeFromSnapshot = null;
 
   componentDidMount () {
     const { updateCollections } = this.props;
     const collectionRef = firestore.collection('collections');
 
-    collectionRef.onSnapshot(async (snapshot) => {
+    this.unsubscribeFromSnapshot = collectionRef.onSnapshot(async (snapshot) => {
       const collectionsMap = convertCollectionsSnapshotToMap(snapshot)
       updateCollections(collectionsMap);
+      this.setState({ loading: false })
     });
   }
   
   render() {
     const { match } = this.props;
+    const { loading } = this.state;
+
     return (
       <div className="shop-page">
-        <Route exact path={`${match.path}`} component={CollectionOverview} />
-        <Route exact path={`${match.path}/:collectionId`} component={CollectionPage} />
+        <Route 
+          exact 
+          path={`${match.path}`} 
+          render={(props) => <CollectionsOverviewWithSpinner isLoading={loading} {...props} /> } 
+        />
+        <Route 
+          exact 
+          path={`${match.path}/:collectionId`} 
+          render={(props) => <CollectionPageWithSpinner isLoading={loading} {...props} /> } 
+        />
       </div>
     )
   }
