a4751e1d7c9f1509037dc6fed41d63c2dee72079
Jakeeeeeeeeeeeee <yangjinchen1995@gmail.com>
2021-07-07 01:00:42 +1000

    feat(GraphQL&Apollo): introducing the GraphQL&Apollo

--- client/src/index.js
+++ client/src/index.js
@@ -8,19 +8,51 @@ import { Provider } from 'react-redux';
 import { store, persistor } from './redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
 import CartProvider from './context/CartProvider/CartProvider';
+import { ApolloProvider } from 'react-apollo';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloClient, gql } from 'apollo-boost';
+
+const httpLink = createHttpLink({
+  uri: 'https://crwn-clothing.com'
+});
+
+const cache = new InMemoryCache();
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache
+});
+
+// client.query({
+//   query: gql`
+//     {
+//       collections {
+//         id 
+//         title
+//         items {
+//           id
+//           price
+//         }
+//       }
+//     }
+//   `
+// }).then(res => console.log(res));
 
 ReactDOM.render(
-  <CartProvider>
-    <Provider store={store}>
-      <React.StrictMode>
-        <BrowserRouter>
-          <PersistGate persistor={persistor} >
-            <App />
-          </PersistGate>
-        </BrowserRouter>
-      </React.StrictMode>
-    </Provider>
-  </CartProvider>
+  <ApolloProvider client={client}>
+    <CartProvider>
+      <Provider store={store}>
+        <React.StrictMode>
+          <BrowserRouter>
+            <PersistGate persistor={persistor} >
+              <App />
+            </PersistGate>
+          </BrowserRouter>
+        </React.StrictMode>
+      </Provider>
+    </CartProvider>
+  </ApolloProvider>
   ,
   document.getElementById('root')
 );
